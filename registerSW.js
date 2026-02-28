34906f40f0b766fcaf4fcb452293d83274e611c2
Dankerowl420 <dunklehass14@gmail.com>
2026-02-27 22:30:10 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lockin/sw.js', { scope: '/lockin/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
